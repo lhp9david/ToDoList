3fe96c2c36bf52345c81a8adca93edd2251a2aa4
David Godard <davidgodard76700@gmail.com>
2023-07-23 17:50:39 +0200

    maj

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let items = [];
 
+
 document.querySelector('#ajouter').addEventListener('click', () => {
     let newDiv = document.createElement('div');
     newDiv.classList.add('produit');
@@ -18,13 +19,13 @@ document.querySelector('#ajouter').addEventListener('click', () => {
     })
 });
 
-window.addEventListener('keypress', (e) => {
+window.addEventListener('keypress', (e) => { 
     if (e.key == 'Enter') {
 
         let newDiv = document.createElement('div');
         newDiv.classList.add('produit');
         newDiv.innerHTML = `<p>${document.querySelector('#item').value}</p><img class="editer" src="img/edit.png" alt=""><img class="supprimer" src="img/bouton-moins.png" alt=""></img>`
-        
+
         document.querySelector('.list').appendChild(newDiv);
 
 
@@ -42,7 +43,7 @@ local.forEach(elt => {
     let newDiv = document.createElement('div');
     newDiv.classList.add('produit');
     newDiv.innerHTML = `<p>${elt}</p><img class="editer" src="img/edit.png" alt=""><img class="supprimer" src="img/bouton-moins.png" alt=""></img>`
-    
+
     items.push(elt)
 
     document.querySelector('.list').appendChild(newDiv);
@@ -54,30 +55,15 @@ document.querySelector('.list').addEventListener("click", event => {
 
     if (event.target.classList.contains("editer")) {
         event.target.previousSibling.contentEditable = true;
-        // event.target.previousSibling.style.backgroundColor = "#dddbdb";
         event.target.previousSibling.focus();
-        // let valider = document.createElement('button')
-        // valider.classList.add('bouton2')
-        // valider.innerText  = 'valider'
-        // document.querySelector('.list').appendChild(valider);
+
         let test = document.querySelectorAll('p');
         let items = [];
         test.forEach(element => {
             items.push(element.innerText)
         })
-        localStorage.setItem('list',JSON.stringify(items))
-        // document.querySelector('.bouton2').addEventListener('click', ()=>{
-        //     event.target.contentEditable = false;
-        //     document.querySelector('.bouton2').remove();
-        //     // event.target.previousSibling.style.backgroundColor = "#fff";
-        //     let test = document.querySelectorAll('p');
-        //     let items = [];
-        //     test.forEach(element => {
-            
-        //         items.push(element.innerText)
-        //     })
-        //     localStorage.setItem('list',JSON.stringify(items))
-        // })
+        localStorage.setItem('list', JSON.stringify(items))
+
     }
 })
 
@@ -87,10 +73,10 @@ document.querySelector('.list').addEventListener("click", event => {
         let test = document.querySelectorAll('p');
         let items = [];
         test.forEach(element => {
-        
+
             items.push(element.innerText)
         })
-        localStorage.setItem('list',JSON.stringify(items))
+        localStorage.setItem('list', JSON.stringify(items))
     }
 })
 
@@ -100,10 +86,10 @@ document.querySelectorAll('p').forEach(element => {
         let test = document.querySelectorAll('p');
         let items = [];
         test.forEach(element => {
-        
+
             items.push(element.innerText)
         })
-        localStorage.setItem('list',JSON.stringify(items))
+        localStorage.setItem('list', JSON.stringify(items))
     })
 })
 
